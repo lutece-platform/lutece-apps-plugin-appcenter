d4244688c2413ab11f4420e7b9775eb3cb9a7e3d
Jon Harper <jon.harper87@gmail.com>
2017-08-17 14:25:38 +0200

    APPCENTER-2 : more fix sql sample data

--- src/sql/plugins/workflow/plugin/init_db_appcenter_workflow.sql
+++ src/sql/plugins/workflow/plugin/init_db_appcenter_workflow.sql
@@ -1,5 +1,5 @@
 INSERT INTO workflow_workflow VALUES 
-(100,'Workflow des demandes d\'accès au site SVN','Workflow des demandes d\'accès au site SVN','2017-08-16 12:38:32',0,'all');
+(100,'Workflow des demandes d\'accès au site SVN','Workflow des demandes d\'accès au site SVN','2017-08-16 12:38:32',1,'all');
 
 
 INSERT INTO workflow_state VALUES 
