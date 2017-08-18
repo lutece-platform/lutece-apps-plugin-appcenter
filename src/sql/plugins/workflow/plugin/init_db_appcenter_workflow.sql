d68df2f3d1099f51be302b32e8bddd35429a4ef5
AlexandreClose <alexandre.close@gmail.com>
2017-08-18 11:34:28 +0200

    APPCENTER-2 : Edit the workflow sql script to add actions and task

--- src/sql/plugins/workflow/plugin/init_db_appcenter_workflow.sql
+++ src/sql/plugins/workflow/plugin/init_db_appcenter_workflow.sql
@@ -1,5 +1,3 @@
-
-
 INSERT INTO workflow_workflow VALUES 
 (100,'Workflow des demandes d\'accès au site SVN','Workflow des demandes d\'accès au site SVN','2017-08-16 12:38:32',1,'all'),
 (200,'Workflow des demandes d\'agents OpenAM','Workflow des demandes d\'agents OpenAM','2017-08-16 12:38:32',1,'all'),
@@ -28,8 +26,15 @@ INSERT INTO workflow_action VALUES
 (100,'Creation de la demande d\'accès au SVN','Creation de la demande d\'accès au SVN',100,100,101,3,1,0,1,0),
 (200,'Creation de la demande d\'agent OpenAM','Creation de la demande d\'agent OpenAM',200,200,201,3,1,0,1,0),
 (300,'Creation de la demande de paramétrage MonCompte','Creation de la demande de paramétrage MonCompte',300,300,301,3,1,0,1,0),
-(400,'Creation de la demande de paramétrage NotifyGRU','Creation de la demande de paramétrage NotifyGRU',400,400,401,3,1,0,1,0);
+(400,'Creation de la demande de paramétrage NotifyGRU','Creation de la demande de paramétrage NotifyGRU',400,400,401,3,1,0,1,0),
 
+(101,'Traiter la demande d\'accès au SVN','Traiter la demande d\'accès au SVN',100,101,102,3,0,0,2,0),
+(201,'Traiter la demande d\'agent OpenAM','Traiter la demande d\'agent OpenAM',200,201,202,3,0,0,2,0),
+(301,'Traiter la demande de paramétrage MonCompte','Traiter la demande de paramétrage MonCompte',300,301,302,3,0,0,2,0),
+(401,'Traiter la demande de paramétrage NotifyGRU','Traiter la demande de paramétrage NotifyGRU',400,401,402,3,0,0,2,0);
+
+INSERT INTO workflow_task VALUES
+(1,'taskOpenam',201,1);
 
 INSERT INTO workflow_resource_workflow VALUES 
 (1,'DEMANDCENTER_DEMAND_SOURCE',101,100,-1,0);
