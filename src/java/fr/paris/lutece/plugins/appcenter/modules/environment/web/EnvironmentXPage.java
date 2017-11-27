02ea3ded28122f714cba7069f6af7ecf1b54dd35
thomas dumont <thomas.dumont@paris.fr>
2017-11-27 14:30:45 +0100

    APPCENTER-18 : add environment demand type

---
+++ src/java/fr/paris/lutece/plugins/appcenter/modules/environment/web/EnvironmentXPage.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2002-2017, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+
+package fr.paris.lutece.plugins.appcenter.modules.environment.web;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import fr.paris.lutece.plugins.appcenter.web.AppCenterXPage;
+import fr.paris.lutece.portal.service.message.SiteMessageException;
+import fr.paris.lutece.portal.service.security.UserNotSignedException;
+import fr.paris.lutece.portal.util.mvc.commons.annotations.View;
+import fr.paris.lutece.portal.util.mvc.xpage.annotations.Controller;
+import fr.paris.lutece.portal.web.xpages.XPage;
+
+/**
+ * EnvironmentXPage
+ */
+@Controller( xpageName = "environment", pageTitleI18nKey = "appcenter.xpage.environment.pageTitle", pagePathI18nKey = "appcenter.xpage.environment.pagePathLabel" )
+public class EnvironmentXPage extends AppCenterXPage
+{
+    private static final String MARK_ENVIRONMENT = "environment";
+
+    // Templates
+    private static final String TEMPLATE_MANAGE_ENVIRONMENT = "/skin/plugins/appcenter/modules/environment/manage_environment.html";
+
+    private static final String VIEW_MANAGE_ENVIRONMENT_DEMANDS = "manageEnvironmentDemands";
+
+
+    /**
+     * Maganage environment view
+     * @param request The HttpServletRequest
+     * @return the view for managing environment
+     * @throws UserNotSignedException
+     * @throws SiteMessageException 
+     */
+    @View( value = VIEW_MANAGE_ENVIRONMENT_DEMANDS, defaultView = true )
+    public XPage getManageNtoficiationGruDemands( HttpServletRequest request ) throws UserNotSignedException, SiteMessageException
+    {
+        Map<String, Object> model = getModel( );
+        fillAppCenterCommons( model, request );
+
+        return getXPage( TEMPLATE_MANAGE_ENVIRONMENT, request.getLocale( ), model );
+    }
+
+    @Override
+    protected String getDemandType()
+    {
+        return "";
+    }
+
+    @Override
+    protected Class getDemandClass()
+    {
+        return null;
+    }
+
+    @Override
+    protected String getDatasName()
+    {
+        return "";
+    }
+
+    @Override
+    protected Class getDatasClass()
+    {
+        return null;
+
+    }
+}
--- src/sql/plugins/appcenter/plugin/init_db_appcenter_sample.sql
+++ src/sql/plugins/appcenter/plugin/init_db_appcenter_sample.sql
@@ -29,30 +29,32 @@ INSERT INTO appcenter_attribute ( id_attribute, key_name, label, description ) V
 
 INSERT INTO appcenter_category_demand_type ( id, label, question , is_depending_of_environment, n_order ) VALUES
     (1,'Applications','Votre application a t-elle besoin des paramétrages de base ?',0,1),
-    (2,'GRU','Votre application a t-elle besoin d''être connectée à la GRU ?',1,2),
-    (3,'Guichet Professionnel','Votre application a t-elle besoin d''être connectée au Guichet Professionnel ?',1,3);
+    (2,'Environnement','Votre application a t-elle besoin des paramétrages propres à chaque environnement ?',1,2),
+    (3,'GRU','Votre application a t-elle besoin d''être connectée à la GRU ?',1,3),
+    (4,'Guichet Professionnel','Votre application a t-elle besoin d''être connectée au Guichet Professionnel ?',1,4);
 
 INSERT INTO appcenter_demand_type ( id, id_demand_type, label, description, question, id_category_demand_type, n_order ) VALUES
     (1,'appcode','Code Applicatif','Le code applicatif est un code sous lequel votre application est enregistrée auprès du BIAD.','Votre application a t-elle besoin d''un code appli ?',1,1),
     (2,'sources','Gestion des sources','Ce type de demande permet de déclarer les référentiels de site et de composants utilisés ou souhaités pour votre application. Il permet également de demander des droits supplémentaires concernant les utilisateurs de ces référentiels.','Avez-vous besoin de créer des réferentiels de sources, ou ajouter des droits sur des référentiels existants ?',1,2),
     (3,'jobs','Intégration continue','Ce type de demande permet de demander la création d''un job Jenkins. Un job Jenkins est un ensemble de traitements automatisés permettant de reconstruire un site ou un composant périodiquement ou après modificiation.','Avez vous besoin de configurer des redéploiements automatiques de votre site ou des composants de celui-ci ? (intégration continue)',1,3),
     (4,'fastdeployapplication','FastDeploy','Fast Deploy est une application permettant de faciliter les déploiements des applications. Il utilise un référentiel de sources, et un environnement, pour déployer l''application sur les machines du BIAD','Souhaitez-vous que votre application utilise l''outil de déploiement simplifié Fast Deploy ?',1,4),
-    (5,'openam','Authentification Front Office','OpenAM est un mode d''authentification qui est notamment utilisé pour authentifier les utilisateurs de la Ville de Paris côté front; dans le cadre de la GRU.','Votre application doit-elle proposer un mode d''authentification "MonCompte" pour l''usager (OpenAM) ?',2,5),
-    (6,'moncomptesettings','Liens Mon Compte','Ce type de demande permet de définir un lien depuis MonCompte vers votre service numérique. Ce lien peut être un favori et/ou un lien de retour depuis la page de profil de MonCompte vers votre service.','Souhaitez-vous définir des liens depuis MonCompte permettant de rejoindre plus facilement votre application (Favori) ?',2,6),
-    (7,'notificationgru','Notifications GRU','Ce type de demande permet d''obtenir la configuration nécessaire afin d''émettre des notifications (Mails, vue 360, MonCompte). L''avantage de ces notifications est qu''elles sont traçées dans la vue 360 et dans MonCompte, ce qui permet un meilleur suivi des demandes des Parisiens','Votre application doit-elle envoyer des notifications vers la GRU, c''est à dire des notifications vers la vue 360, le compte parisien des usagers, ou envoyer des mails à ces derniers ?',2,7),
-    (8,'identitystore','Référentiel d''identité','Ce type de demande permet d''obtenir les configurations nécessaires auprès du référentiel d''identité de la GRU. Il s''agit ici de déterminer quels sont les droits de votre application sur les attributs des usagers','Votre application doit-elle récupérer des attributs de l''usager qui se connecte ?',2,8),
-    (9,'guichetpro','Guichet professionnel','Ce type de demande permet d''obtenir les configurations nécessaires afin d''émettre des notifications vers le Guichet Professionnel','Votre application a t-elle besoin d''émettre des notifications vers le Guichet Professionnel ?',3,9);
+    (5,'environment','Création de l''environment','Ce type de demande permet d''obtenir les configurations nécessaires afin de créer l''environement applicatif','Votre application a t-elle besoin d''un environnement applicatif ?',2,5),
+    (6,'openam','Authentification Front Office','OpenAM est un mode d''authentification qui est notamment utilisé pour authentifier les utilisateurs de la Ville de Paris côté front; dans le cadre de la GRU.','Votre application doit-elle proposer un mode d''authentification "MonCompte" pour l''usager (OpenAM) ?',3,6),
+    (7,'moncomptesettings','Liens Mon Compte','Ce type de demande permet de définir un lien depuis MonCompte vers votre service numérique. Ce lien peut être un favori et/ou un lien de retour depuis la page de profil de MonCompte vers votre service.','Souhaitez-vous définir des liens depuis MonCompte permettant de rejoindre plus facilement votre application (Favori) ?',3,7),
+    (8,'notificationgru','Notifications GRU','Ce type de demande permet d''obtenir la configuration nécessaire afin d''émettre des notifications (Mails, vue 360, MonCompte). L''avantage de ces notifications est qu''elles sont traçées dans la vue 360 et dans MonCompte, ce qui permet un meilleur suivi des demandes des Parisiens','Votre application doit-elle envoyer des notifications vers la GRU, c''est à dire des notifications vers la vue 360, le compte parisien des usagers, ou envoyer des mails à ces derniers ?',3,8),
+    (9,'identitystore','Référentiel d''identité','Ce type de demande permet d''obtenir les configurations nécessaires auprès du référentiel d''identité de la GRU. Il s''agit ici de déterminer quels sont les droits de votre application sur les attributs des usagers','Votre application doit-elle récupérer des attributs de l''usager qui se connecte ?',3,9),
+    (10,'guichetpro','Guichet professionnel','Ce type de demande permet d''obtenir les configurations nécessaires afin d''émettre des notifications vers le Guichet Professionnel','Votre application a t-elle besoin d''émettre des notifications vers le Guichet Professionnel ?',4,10);
 
 INSERT INTO appcenter_documentation( id_documentation, id_demand_type, label, url, category ) VALUES
     (1,4,'Mode d''emploi de l''application Fast Deploy','http://fr.lutece.paris.fr/fr/wiki/fastdeploy.html','project_manager'),
     (2,4,'FAQ Fast Deploy','http://fr.lutece.paris.fr/fr/wiki/faq-fastdeploy.html','project_manager'),
-    (3,5,'Présentation de l''authentification Front apportée par la GRU','http://fr.lutece.paris.fr/fr/wiki/gru-authentication.html','project_manager'),
-    (4,5,'Configuration de l''authentification Front apportée par la GRU','http://fr.lutece.paris.fr/fr/wiki/configluteceagentopenam.html','integ'),
-    (5,6,'Personnalisation des liens vers MonCompte','http://fr.lutece.paris.fr/fr/wiki/gru-proposecertificationinservices.html','dev'),
-    (6,7,'Comment envoyer une notification GRU ?','http://fr.lutece.paris.fr/fr/wiki/gru-notifygru.html','dev'),
-    (7,8,'Comment récupérer des attributs utilisateurs ? Première méthode','http://fr.lutece.paris.fr/fr/wiki/gru-loadattributesafterauthentication.html','integ'),
-    (8,8,'Comment récupérer des attributs utilisateurs ? Deuxième méthode','http://fr.lutece.paris.fr/fr/wiki/gru-library-identitystore.html','dev'),
-    (9,8,'Comment récupérer des attributs utilisateurs ? Troisième méthode','http://fr.lutece.paris.fr/fr/wiki/gru-appeldirect-identitystore.html','dev');
+    (3,6,'Présentation de l''authentification Front apportée par la GRU','http://fr.lutece.paris.fr/fr/wiki/gru-authentication.html','project_manager'),
+    (4,6,'Configuration de l''authentification Front apportée par la GRU','http://fr.lutece.paris.fr/fr/wiki/configluteceagentopenam.html','integ'),
+    (5,7,'Personnalisation des liens vers MonCompte','http://fr.lutece.paris.fr/fr/wiki/gru-proposecertificationinservices.html','dev'),
+    (6,8,'Comment envoyer une notification GRU ?','http://fr.lutece.paris.fr/fr/wiki/gru-notifygru.html','dev'),
+    (7,9,'Comment récupérer des attributs utilisateurs ? Première méthode','http://fr.lutece.paris.fr/fr/wiki/gru-loadattributesafterauthentication.html','integ'),
+    (8,9,'Comment récupérer des attributs utilisateurs ? Deuxième méthode','http://fr.lutece.paris.fr/fr/wiki/gru-library-identitystore.html','dev'),
+    (9,9,'Comment récupérer des attributs utilisateurs ? Troisième méthode','http://fr.lutece.paris.fr/fr/wiki/gru-appeldirect-identitystore.html','dev');
 
 INSERT INTO appcenter_demand_type_category ( id_demand_type_category, name, description ) VALUES
     (1,'Autorisation d\'occupation du domaine public','Autorisation d\'occupation du domaine public'),
