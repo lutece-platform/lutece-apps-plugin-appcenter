676e46fbc9f8cca53b9b3f23e9bc726bd6104251
thomas dumont <thomas.dumont@paris.fr>
2017-11-27 14:55:00 +0100

    APPCENTER-19 : add wsso demand type

--- src/sql/plugins/appcenter/plugin/init_db_appcenter_sample.sql
+++ src/sql/plugins/appcenter/plugin/init_db_appcenter_sample.sql
@@ -31,7 +31,8 @@ INSERT INTO appcenter_category_demand_type ( id, label, question , is_depending_
     (1,'Applications','Votre application a t-elle besoin des paramétrages de base ?',0,1),
     (2,'Environnement','Votre application a t-elle besoin des paramétrages propres à chaque environnement ?',1,2),
     (3,'GRU','Votre application a t-elle besoin d''être connectée à la GRU ?',1,3),
-    (4,'Guichet Professionnel','Votre application a t-elle besoin d''être connectée au Guichet Professionnel ?',1,4);
+    (4,'Guichet Professionnel','Votre application a t-elle besoin d''être connectée au Guichet Professionnel ?',1,4),
+    (5,'WSSO','Votre application a t-elle besoin d''être rattachée au WSSO ?',1,5);
 
 INSERT INTO appcenter_demand_type ( id, id_demand_type, label, description, question, id_category_demand_type, n_order ) VALUES
     (1,'appcode','Code Applicatif','Le code applicatif est un code sous lequel votre application est enregistrée auprès du BIAD.','Votre application a t-elle besoin d''un code appli ?',1,1),
@@ -43,7 +44,8 @@ INSERT INTO appcenter_demand_type ( id, id_demand_type, label, description, ques
     (7,'moncomptesettings','Liens Mon Compte','Ce type de demande permet de définir un lien depuis MonCompte vers votre service numérique. Ce lien peut être un favori et/ou un lien de retour depuis la page de profil de MonCompte vers votre service.','Souhaitez-vous définir des liens depuis MonCompte permettant de rejoindre plus facilement votre application (Favori) ?',3,7),
     (8,'notificationgru','Notifications GRU','Ce type de demande permet d''obtenir la configuration nécessaire afin d''émettre des notifications (Mails, vue 360, MonCompte). L''avantage de ces notifications est qu''elles sont traçées dans la vue 360 et dans MonCompte, ce qui permet un meilleur suivi des demandes des Parisiens','Votre application doit-elle envoyer des notifications vers la GRU, c''est à dire des notifications vers la vue 360, le compte parisien des usagers, ou envoyer des mails à ces derniers ?',3,8),
     (9,'identitystore','Référentiel d''identité','Ce type de demande permet d''obtenir les configurations nécessaires auprès du référentiel d''identité de la GRU. Il s''agit ici de déterminer quels sont les droits de votre application sur les attributs des usagers','Votre application doit-elle récupérer des attributs de l''usager qui se connecte ?',3,9),
-    (10,'guichetpro','Guichet professionnel','Ce type de demande permet d''obtenir les configurations nécessaires afin d''émettre des notifications vers le Guichet Professionnel','Votre application a t-elle besoin d''émettre des notifications vers le Guichet Professionnel ?',4,10);
+    (10,'guichetpro','Guichet professionnel','Ce type de demande permet d''obtenir les configurations nécessaires afin d''émettre des notifications vers le Guichet Professionnel','Votre application a t-elle besoin d''émettre des notifications vers le Guichet Professionnel ?',4,10),
+    (11,'wsso','WSSO','Ce type de demande permet de bénéficier de l''authentification WSSO','Votre application doit-elle être ratachée au mode d''authentification WSSO ?',5,11);
 
 INSERT INTO appcenter_documentation( id_documentation, id_demand_type, label, url, category ) VALUES
     (1,4,'Mode d''emploi de l''application Fast Deploy','http://fr.lutece.paris.fr/fr/wiki/fastdeploy.html','project_manager'),
@@ -54,7 +56,8 @@ INSERT INTO appcenter_documentation( id_documentation, id_demand_type, label, ur
     (6,8,'Comment envoyer une notification GRU ?','http://fr.lutece.paris.fr/fr/wiki/gru-notifygru.html','dev'),
     (7,9,'Comment récupérer des attributs utilisateurs ? Première méthode','http://fr.lutece.paris.fr/fr/wiki/gru-loadattributesafterauthentication.html','integ'),
     (8,9,'Comment récupérer des attributs utilisateurs ? Deuxième méthode','http://fr.lutece.paris.fr/fr/wiki/gru-library-identitystore.html','dev'),
-    (9,9,'Comment récupérer des attributs utilisateurs ? Troisième méthode','http://fr.lutece.paris.fr/fr/wiki/gru-appeldirect-identitystore.html','dev');
+    (9,9,'Comment récupérer des attributs utilisateurs ? Troisième méthode','http://fr.lutece.paris.fr/fr/wiki/gru-appeldirect-identitystore.html','dev'),
+    (10,11,'Comment faire une demande WSSO sur SATIS ?','http://intraparis.dsti.mdp/DSTI/document?id=10845&id_attribute=127','project_manager');
 
 INSERT INTO appcenter_demand_type_category ( id_demand_type_category, name, description ) VALUES
     (1,'Autorisation d\'occupation du domaine public','Autorisation d\'occupation du domaine public'),
