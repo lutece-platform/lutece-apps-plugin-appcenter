8188b28140cd995c3a7589bbc4d8c9d135e8c1a3
FrancoisEricMerlin <francoiseric.merlin@paris.fr>
2017-12-06 17:25:32 +0100

    APPCENTER-14 : create an update of application code when demand is closed

--- src/java/fr/paris/lutece/plugins/appcenter/modules/appcode/service/AppCodeTask.java
+++ src/java/fr/paris/lutece/plugins/appcenter/modules/appcode/service/AppCodeTask.java
@@ -4,10 +4,19 @@ import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
+import fr.paris.lutece.plugins.appcenter.business.Application;
+import fr.paris.lutece.plugins.appcenter.business.ApplicationHome;
+import fr.paris.lutece.plugins.appcenter.business.Demand;
+import fr.paris.lutece.plugins.appcenter.business.DemandHome;
 import fr.paris.lutece.plugins.appcenter.modules.appcode.business.AppCodeData;
 import fr.paris.lutece.plugins.appcenter.modules.appcode.business.AppCodeDatas;
 import fr.paris.lutece.plugins.appcenter.modules.appcode.business.AppCodeDemand;
+import fr.paris.lutece.plugins.appcenter.modules.identitystore.business.IdentitystoreData;
+import fr.paris.lutece.plugins.appcenter.modules.identitystore.business.IdentitystoreDatas;
+import fr.paris.lutece.plugins.appcenter.modules.identitystore.business.IdentitystoreDemand;
+import fr.paris.lutece.plugins.appcenter.service.task.AppCenterTaskFunctional;
 import fr.paris.lutece.plugins.appcenter.service.task.AppcenterTask;
+import fr.paris.lutece.plugins.workflowcore.business.resource.ResourceHistory;
 /**
  * 
  *  AppCodeTask
@@ -28,7 +37,24 @@ public class AppCodeTask extends AppcenterTask
     @Override
     public void processTask( int nIdResourceHistory, HttpServletRequest request, Locale locale )
     {
-           super.processTask(nIdResourceHistory, request, locale, AppCodeData.class, AppCodeDatas.class, AppCodeDemand.class);
+           
+        AppCenterTaskFunctional<AppCodeData, AppCodeDatas, AppCodeDemand> funct=( requestParam,  localeParam, applicationDataParam, applicationDatasParam,demandParam)-> updateAppCode( applicationDataParam, nIdResourceHistory );
+         super.processTask(nIdResourceHistory, request, locale, AppCodeData.class, AppCodeDatas.class, AppCodeDemand.class,funct);
 
     }
+    
+    
+    private void updateAppCode(AppCodeData appCodeData,int nIdResourceHistory)
+    {
+        ResourceHistory resourceHistory = super._resourceHistoryService.findByPrimaryKey( nIdResourceHistory );
+        Demand demand = DemandHome.findByPrimaryKey( resourceHistory.getIdResource( ) );
+
+        Application application = ApplicationHome.findByPrimaryKey( demand.getIdApplication( ) );
+        application.setCode( appCodeData.getApplicationCode( ) );
+        ApplicationHome.update( application );
+        
+    }
+        
+    
+    
 }
