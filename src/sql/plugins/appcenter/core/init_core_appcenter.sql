e1692bd2da0e2f8639e91dbc6b49eba333ba7f0f
thomas dumont <thomas.dumont@paris.fr>
2018-02-19 12:28:38 +0100

    APPCENTER-1 : Change first view to ManageDemands for Manage Appcenter admin feature

--- src/sql/plugins/appcenter/core/init_core_appcenter.sql
+++ src/sql/plugins/appcenter/core/init_core_appcenter.sql
@@ -4,7 +4,7 @@
 --
 DELETE FROM core_admin_right WHERE id_right = 'APPCENTER_MANAGEMENT';
 INSERT INTO core_admin_right (id_right,name,level_right,admin_url,description,is_updatable,plugin_name,id_feature_group,icon_url,documentation_url, id_order ) VALUES 
-('APPCENTER_MANAGEMENT','appcenter.adminFeature.ManageAppCenter.name',1,'jsp/admin/plugins/appcenter/ManageApplications.jsp','appcenter.adminFeature.ManageAppCenter.description',0,'appcenter',NULL,NULL,NULL,4);
+('APPCENTER_MANAGEMENT','appcenter.adminFeature.ManageAppCenter.name',1,'jsp/admin/plugins/appcenter/ManageDemands.jsp','appcenter.adminFeature.ManageAppCenter.description',0,'appcenter',NULL,NULL,NULL,4);
 
 
 --
